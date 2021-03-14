06c57a88d96bd29e76c68d8a5414ddadc02998d0
Dermot Duffy <dermot.duffy@gmail.com>
2021-03-14 10:43:47 -0700

    Expand test coverage.

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,11 +2,34 @@
 
 import logging
 from aiohttp import web  # type: ignore
-from typing import Any
+from typing import Any, List
 
 from motioneye_client.client import MotionEyeClient
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def _create_motioneye_server(aiohttp_server: Any, handlers: List[Any]) -> Any:
+    app = web.Application()
+    app.add_routes(handlers)
+    return await aiohttp_server(app)
+
+
+async def test_signature(aiohttp_server: Any) -> None:
+    """Test client login."""
+
+    async def login_handler(request: web.Request) -> web.Response:
+        assert "_signature" in request.query
+        assert request.query["_signature"] == "2df0446590ac6038c4cec2b3d39639bf22575fed"
+        return web.json_response({})
+
+    server = await _create_motioneye_server(
+        aiohttp_server, [web.get("/login", login_handler)]
+    )
+
+    client = MotionEyeClient(str(server.make_url("/")), "username", "password")
+    assert await client.async_client_login()
+    assert await client.async_client_close()
 
 
 async def test_login(aiohttp_server: Any) -> None:
@@ -15,9 +38,9 @@ async def test_login(aiohttp_server: Any) -> None:
     async def login_handler(request: web.Request) -> web.Response:
         return web.json_response({"moo": "foo"})
 
-    app = web.Application()
-    app.add_routes([web.get("/login", login_handler)])
-    server = await aiohttp_server(app)
+    server = await _create_motioneye_server(
+        aiohttp_server, [web.get("/login", login_handler)]
+    )
 
     client = MotionEyeClient(str(server.make_url("/")), "username", "password")
     assert await client.async_client_login()
