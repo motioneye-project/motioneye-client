74c27fce585a71dcd61dbc60c93aa00529225b55
Marcin Krawczyk <mkc@kramar.pl>
2026-09-15 21:53:15 +0200

    Preserve 416 responses for media streaming

--- motioneye_client/client.py
+++ motioneye_client/client.py
@@ -192,7 +192,7 @@ class MotionEyeClient:
                     _LOGGER.warning(f"Authentication failed in request to {url}")
                     raise MotionEyeClientInvalidAuthError(response)
 
-                if not response.ok:
+                if not response.ok and response.status != 416:
                     _LOGGER.warning(
                         f"Unexpected HTTP response status code {response.status} for request: {url}"
                     )
