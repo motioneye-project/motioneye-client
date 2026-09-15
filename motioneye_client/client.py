5ec32627b2747e0574448136668cac7229036761
Marcin Krawczyk <mkc@kramar.pl>
2026-09-15 22:11:04 +0200

    Remove trailing whitespace

--- motioneye_client/client.py
+++ motioneye_client/client.py
@@ -72,7 +72,7 @@ class MotionEyeClientMediaResponse:
     def content(self) -> aiohttp.StreamReader:
         """Return the streaming response body."""
         return self.response.content
-    
+
 
 class MotionEyeClient:
     """MotionEye Client."""
